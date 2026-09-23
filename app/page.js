b0713b83aa33caf02e2e3457e1200322aa71f1d9
zlxl090411-create <zlxl090411@gmail.com>
2026-09-24 03:10:58 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 const DISPOSAL_DAYS = 90;
 
