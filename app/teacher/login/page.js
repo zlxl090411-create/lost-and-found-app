e24b088955fa83a085a1f217375fce495861ba76
zlxl090411-create <zlxl090411@gmail.com>
2026-09-24 19:40:58 +0900

    Update page.js

--- app/teacher/login/page.js
+++ app/teacher/login/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../../../lib/supabaseClient';
 
 export default function TeacherLoginPage() {
   const [email, setEmail] = useState('');
